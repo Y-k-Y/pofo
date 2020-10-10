c6ceae09372b01fcc269362439779d95f497b264
Y-k-Y <Zzzzz.co.kr@gmail.com>
2020-10-11 02:07:24 +0900

    test netlify

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,7 +4,7 @@ class Portfolio extends Component {
   render() {
     if (this.props.data) {
       var projects = this.props.data.projects.map(function (projects) {
-        var projectImage = "images/portfolio/" + projects.image;
+        // var projectImage = "images/portfolio/" + projects.image;
         var description = projects.description;
         var descriptionList = ``;
 
